2d20dd91f3890ad3880da36def84dc163123b86a
unknown <ghanshyam.rak127@gmail.com>
2021-07-08 13:39:08 +0530

    calculator added by ghanshyam

---
+++ python_files/calculator.py
@@ -0,0 +1,19 @@
+def addition(n1, n2):
+    ans = n1 + n2
+    print(ans)
+
+
+def subtraction(n1, n2):
+    ans = n1 - n2
+    print(ans)
+
+
+def multipliction(n1, n2):
+    ans = n1 * n2
+    print(ans)
+
+
+def divide(n1, n2):
+    ans = n1 / n2
+    print(ans)
+
